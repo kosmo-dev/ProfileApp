a7060f14a09076a43f7b3ffc124c4e8445fc9854
Vadim Kuzmin <kuzminvadima@yandex.ru>
2023-08-01 12:05:58 +0300

    Made viewController layout

---
+++ ProfileApp/UIColor+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  UIColor+Extension.swift
+//  ProfileApp
+//
+//  Created by Вадим Кузьмин on 01.08.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static let background = UIColor(named: "BackgroundColor")
+}
--- ProfileApp/ViewController.swift
+++ ProfileApp/ViewController.swift
@@ -7,10 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    // MARK: - Private Properties
+    private var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero)
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
 
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureView()
+        configureConstraints()
+    }
+
+    // MARK: - Private Methods
+    private func configureView() {
+        navigationItem.title = "Profile"
+        view.backgroundColor = .background
+        view.addSubview(collectionView)
+    }
+
+    private func configureConstraints() {
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+
+    private func registerCells() {
+        
     }
 }
 
